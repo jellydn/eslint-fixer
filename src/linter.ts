177dac801c52a8e3f0988010a6b393a696858833
Huynh Duc Dung <dunghd.it@gmail.com>
2024-07-11 21:06:42 +0800

    feat(linter): enable auto-fixing of ESLint issues

--- src/linter.ts
+++ src/linter.ts
@@ -2,11 +2,15 @@ import { ESLint } from "eslint";
 
 export async function eslintFixer(patterns: string) {
 	const eslint = new ESLint({
+		fix: true,
 		overrideConfigFile: require.resolve("./eslint.config.js"),
 		errorOnUnmatchedPattern: false,
 	});
 
 	const results = await eslint.lintFiles(patterns.split(","));
+
+	// Autofixes lint problems
+	await ESLint.outputFixes(results);
 
 	const formatter = await eslint.loadFormatter("stylish");
 	const resultText = formatter.format(results);
